226efd1e58e71580653cfb9766aa3c16ea1d5955
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2024-09-13 15:57:06 +0200

    java
    java

---
+++ MasterMind/src/Mastermind.java
@@ -0,0 +1,14 @@
+
+public class Mastermind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String rood = "rd";
+String blauw = "bl";
+String geel = "gl";
+String groen = "gr";
+String paars = "ps";
+
+	}
+	
+}

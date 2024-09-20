0638ffde4071481226bbfc4e9e34aeb80e75e03f
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2024-09-20 13:48:34 +0200

    kip
    ei

--- MasterMind/src/Mastermind.java
+++ MasterMind/src/Mastermind.java
@@ -3,12 +3,21 @@ public class Mastermind {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String rood = "rd";
+String rood = "ro";
 String blauw = "bl";
 String geel = "gl";
 String groen = "gr";
 String paars = "ps";
+String wit = "wi";
+String zward = "zw";
 // TODO variabele vakjes
 	}
 	
 }
+
+
+
+
+
+
+
---
+++ UserInput/src/chickens01/Chickens01.java
@@ -0,0 +1,46 @@
+
+package chickens01;
+
+public class Chickens01 {
+    public static void main(String[] args) {
+        //Put yout code here
+    	int egg = 5;
+    	int chi = 3;
+    	
+    	int egg2 = 5;
+    	int chi2 = 4;
+    	
+    	int egg3 = 5;
+    	int chi3 = 2;
+    	
+    	String Dag1 = "Dag1";
+    	
+    	System.out.println(Dag1);
+    	
+    	int som = chi * egg;
+    	int som2 = chi2 * egg2;
+    	int som3 = chi3 * egg3;
+    	
+    	 System.out.println("er zijn " + chi + " kippen en die leggen per kip " + egg + " eieren. dus ze leggen in totaal zo veel eieren : " + som);
+    	 
+    	 String Dag2 = "Dag2";
+    	
+    	 System.out.println(Dag2);
+    	 
+    	 System.out.println("er komt nu 1 kip bij dus zijn er " + chi2 + " kippen en die leggen per kip " + egg2 + " eieren. dus ze leggen in totaal zo veel eieren : " + som2);
+    	 
+    	 String Dag3 = "Dag3";
+     	
+    	 System.out.println(Dag3);
+    	 
+    	 System.out.println("de helft van de kippen zijn door een vos opgegeten " + chi3 + " kippen en die leggen per kip " + egg3 + " eieren. dus ze leggen in totaal zo veel eieren : " + som3);
+    	 
+    	 
+    	 
+    	 
+    	
+    	 
+    	
+
+    }   
+}

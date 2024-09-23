4fd6d0a4a9401df168c0a489739b5e87ab620a46
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2024-09-23 14:44:23 +0200

    java
    java

--- MasterMind/src/Mastermind.java
+++ MasterMind/src/Mastermind.java
@@ -3,14 +3,16 @@ public class Mastermind {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String rood = "ro";
-String blauw = "bl";
-String geel = "gl";
-String groen = "gr";
-String paars = "ps";
-String wit = "wi";
-String zward = "zw";
+String ro = "Rood";
+String bl = "blauw";
+String gl = "geel";
+String gr = "groen";
+String ps = "paars";
+String or = "oranje";
+String wi = "wit";
+String zw = "zward";
 // TODO variabele vakjes
+
 	}
 	
 }
---
+++ UserInput/src/input03/Test.java
@@ -0,0 +1,15 @@
+package input03;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 3;
+
+double y = 3.0;
+
+System.out.println(x == y);
+	}
+
+}
+
